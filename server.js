a3fb137fe491a5605684eec06a3b87f1dc585191
roushanj <roushan.jha0709@gmail.com>
2017-03-07 21:05:31 +0530

    main server

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var main = require('./routes/main');
+var memories = require('./routes/memories');
+
+var app = express();
+var port = 3000;
+
+
+app.set('views',path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
+
+app.use(express.static(path.join(__dirname, ('client'))));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extented: false}));
+
+app.use('/', main);
+app.use('/user', memories);
+
+
+
+app.listen(port, function() {
+
+	console.log('server is listening');
+});
