4065005a63edffcf2029d765b8811d053fabfab4
roushanj <roushan.jha0709@gmail.com>
2017-03-16 20:11:43 +0530

    added new functions to auth

--- models/user.js
+++ models/user.js
@@ -1,8 +1,6 @@
 var mongoose = require('mongoose');
 var bcryptjs = require('bcryptjs');
 
-
-
 //user schema
 
 var userSchema = mongoose.Schema({
@@ -31,3 +29,24 @@ module.exports.createUser = function (newUser, callback) {
     newUser.save(callback);
 });
 }
+
+module.exports.getuserbyUsername=function(user, callback) {
+	var query = {username:username};
+	User.findOne(query, callback);
+};
+module.exports.comparePassword=function(candidatePassword, hash, callback) {
+	 bcryptjs.compare(candidatePassword,hash,function (err, callback) {
+
+	 	if (err) {throw err};
+	 	callback(null , isMatch);
+	 })
+};  
+
+
+module.exports.getuseryId=function(id, callback) {
+
+
+	User.findById(id, callback);
+	
+
+};
